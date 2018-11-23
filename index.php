3847a241d75a4ab9d2fb01ed78448fcdc1d570f4
wildan <wildanfajar2017@mail.ugm.ac.id>
2018-11-23 22:47:29 +0700

    pls

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     else if(strtolower($event['message']['text']) == '/list'){
                         $carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
                             new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Survey Nama Taman Baru", "Vote untuk nama taman baru!","https://travelyuk.files.wordpress.com/2010/06/butchard.jpg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Open Survey 1',"http://hilite.me/"),
+                            new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('Open Survey 1',"Open Survey 1", "Open Survey 1"),
                             ]),
                             new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Survey Kebersihan Selokan Mataram", "Rate kebersihan Selokan Mataram!","https://s.kaskus.id/images/2015/06/20/7853087_20150620063627.jpg",[
                             new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Open Survey 2',"http://hilite.me/"),
